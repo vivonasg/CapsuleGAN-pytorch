d8773411ac00f74a091954927abc3016fbdc3eb8
vivonasg <>
2018-04-27 12:57:44 -0400

    added rotated imag

--- capsgan_hyperparam_search.py
+++ capsgan_hyperparam_search.py
@@ -24,7 +24,29 @@ opt = parser.parse_args()
 param_hyper=[[0.9],[0.1],[0.5],[0.005]]
 lr_hyper=[0.002]
 SN_hyper=[False]
-CAPS_bool=[False]
+CAPS_bool=[False,True]
+rotate_bool=[True]
+rotate_angle=[30]
+
+'''
+def run_model(lr=0.002,
+            batch_size=64,
+            train_epoch= 20,
+            img_size=32, 
+            SN_bool=True, 
+            D_param=[0.9,0.1,0.5,0.005],
+            reconstruction_loss_bool=True, 
+            USE_CAPS_D=True, 
+            SAVE_TRAINING=True, 
+            SAVE_IMAGE=True, 
+            num_iter_limit=5, 
+            verbose=True, 
+            train_loader=None, 
+            hyperparam_tag='1', 
+            rotate_bool=True, 
+            rotate_degree_range=45):
+'''
+
 
 train_loader=get_data(batch_size=opt.b)
 
@@ -35,15 +57,34 @@ for param_0 in param_hyper[0]:
 				for lr in lr_hyper:
 					for SN in SN_hyper:
 						for CAPS in CAPS_bool:
+							for rotate in rotate_bool:
+								hyper_tag=str(param_0)+'-'+str(param_1)+'-'+str(param_2)+'-'+str(param_3)+'-'+str(lr)+'-'+str(SN)
+								if rotate:
+									for angle in rotate_angle:			
+										print(hyper_tag)
+										print('angle: ', angle)
+										run_model(lr=lr,
+								            SN_bool=SN, 
+								            D_param=[param_0,param_1,param_2,param_3],
+								            num_iter_limit=opt.n,
+								            hyperparam_tag=hyper_tag,
+								            train_loader=train_loader,
+								            USE_CAPS_D=CAPS, 
+								            rotate_bool=rotate,
+								            rotate_degree_range=angle
+								            )
+								else: 		
+									print(hyper_tag)
+									run_model(lr=lr,
+							            SN_bool=SN, 
+							            D_param=[param_0,param_1,param_2,param_3],
+							            num_iter_limit=opt.n,
+							            hyperparam_tag=hyper_tag,
+							            train_loader=train_loader,
+							            USE_CAPS_D=CAPS, 
+							            rotate_bool=rotate,
+							            rotate_degree_range=0
+							            )
 
-							hyper_tag=str(param_0)+'-'+str(param_1)+'-'+str(param_2)+'-'+str(param_3)+'-'+str(lr)+'-'+str(SN)
-							
-							print(hyper_tag)
 
-							run_model(lr=lr,
-					            SN_bool=SN, 
-					            D_param=[param_0,param_1,param_2,param_3],
-					            num_iter_limit=opt.n,
-					            hyperparam_tag=hyper_tag,
-					            train_loader=train_loader,
-					            USE_CAPS_D=CAPS)
+
--- pytorch_MNIST_CAPS_DCGAN.py
+++ pytorch_MNIST_CAPS_DCGAN.py
@@ -11,6 +11,9 @@ import torchvision.utils as vutils
 from capsule_network import *
 import argparse
 import pdb
+import matplotlib.pyplot as plt
+import random
+import cv2
 
 USE_CUDA=torch.cuda.is_available()
 
@@ -58,16 +61,26 @@ class generator(nn.Module):
 
 class discriminator(nn.Module):
     # initializers
-    def __init__(self, d=128,img_size=32):
+    def __init__(self, d=128,img_size=32,dataset='mnist'):
         super(discriminator, self).__init__()
+        self.output_channels=1
+        if dataset=='cifar10':
+            self.output_channels=3
+
         self.img_size=img_size
-        self.conv1 = nn.Conv2d(1, d*2, 5)
-        self.conv2 = nn.Conv2d(d*2, d*2, 5)
-        self.conv3 = nn.Conv2d(d*2, d, 5)
+        self.conv1 = nn.Conv2d(self.output_channels, d, 4, 2, 1)
+        self.conv2 = nn.Conv2d(d, d*2, 4, 2, 1)
+        self.conv2_bn = nn.BatchNorm2d(d*2)
+        self.conv3 = nn.Conv2d(d*2, d*4, 4, 2, 1)
+        self.conv3_bn = nn.BatchNorm2d(d*4)
+        self.conv4 = nn.Conv2d(d*4, d*8, 4, 2, 1)
         
-        self.fc1 = nn.Linear(d*20*20,328)
-        self.fc2 = nn.Linear(328,192)
-        self.fc3 = nn.Linear(192,1)
+        if self.img_size==64:
+            self.conv4 = nn.Conv2d(d*4, d*8, 4, 2, 1)
+            self.conv4_bn = nn.BatchNorm2d(d*8)
+            self.conv5 = nn.Conv2d(d*8, 1, 4, 1, 0)
+        if self.img_size==32:
+            self.conv4 = nn.Conv2d(d*4,1,4,1,0)
 
     # weight_init
     def weight_init(self, mean, std):
@@ -76,22 +89,16 @@ class discriminator(nn.Module):
 
     # forward method
     def forward(self, input):
-        x = F.leaky_relu(self.conv1(input),0.2)
-        x = F.leaky_relu(self.conv2(x),0.2)
-        x = F.leaky_relu(self.conv3(x),0.2)
-
-        x=x.view(-1,self.num_flat_features(x))
-        x= F.leaky_relu(self.fc1(x),0.2)
-        x= F.leaky_relu(self.fc2(x),0.2)
-        x= F.sigmoid(self.fc3(x))
+        x = F.leaky_relu(self.conv1(input), 0.2)
+        x = F.leaky_relu(self.conv2_bn(self.conv2(x)), 0.2)
+        x = F.leaky_relu(self.conv3_bn(self.conv3(x)), 0.2)
+        if self.img_size==64:
+            x = F.leaky_relu(self.conv4_bn(self.conv4(x)), 0.2)
+            x = F.sigmoid(self.conv5(x))
+        if self.img_size==32:
+            x=F.sigmoid(self.conv4(x))
         return x
 
-    def num_flat_features(self,x):
-        size= x.size()[1:]
-        num_features = 1
-        for s in size:
-            num_features *= s
-        return num_features
 
 
 def normal_init(m, mean, std):
@@ -139,6 +146,29 @@ def get_data(batch_size=64,i_size=32):
         batch_size=batch_size, shuffle=True)
     return train_loader
 
+def rotate_batch(img_batch,degree_angle=0,random_bool=False,random_range=[-45,45]):
+    img_batch=img_batch.numpy()
+    cols, rows= img_batch.shape[2:4]
+    background=np.zeros((cols*2,rows*2))
+    background.fill(-1)
+
+    for i in range(len(
+        img_batch)):
+        background[int(cols*0.5):int(cols*0.5)+cols,int(rows*0.5):int(rows*0.5)+rows]=img_batch[i,0,:,:]
+        img=background
+        if random_bool:
+            degree_angle=random.uniform(random_range[0],random_range[1])
+
+        cols2,rows2= img.shape
+        M=cv2.getRotationMatrix2D((cols2/2,rows2/2),degree_angle,1)
+        img = cv2.warpAffine(img,M,(cols2,rows2))
+        img_batch[i,0,:,:] = img[int(cols*0.5):int(cols*0.5)+cols,int(rows*0.5):int(rows*0.5)+rows]
+        
+        plt.imshow(img_batch[i,0,:,:])
+      
+    return torch.from_numpy(img_batch)
+
+
 
 def run_model(lr=0.002,
             batch_size=64,
@@ -153,7 +183,10 @@ def run_model(lr=0.002,
             num_iter_limit=5, 
             verbose=True, 
             train_loader=None, 
-            hyperparam_tag='1'):
+            hyperparam_tag='1', 
+            rotate_bool=True, 
+            rotate_degree_range=45):
+
 
     # network
     if USE_CAPS_D:
@@ -210,7 +243,11 @@ def run_model(lr=0.002,
             D.zero_grad()
 
             mini_batch = x_.size()[0]
-           
+
+            if rotate_bool:
+                x_=rotate_batch(img_batch=x_,random_bool=True,random_range=[-rotate_degree_range,rotate_degree_range])
+
+
             y_real_ = torch.ones(mini_batch)
             y_fake_ = torch.zeros(mini_batch)
             if USE_CUDA:
@@ -268,9 +305,10 @@ def run_model(lr=0.002,
 
             G_losses.append(G_train_loss.data[0])
             num_iter += 1
-
+             
+            rotate_tag='_rotate_'+str(rotate_degree_range) if rotate_bool else ''
             D_tag= 'CAPS' if USE_CAPS_D else 'BASE'
-            tag= hyperparam_tag +"_"+ str(num_iter) + '_size_'+str(img_size)+"_bs_"+str(batch_size)+D_tag
+            tag= hyperparam_tag +"_"+ str(num_iter) + '_size_'+str(img_size)+"_bs_"+str(batch_size)+D_tag+rotate_tag
 
             iter_end_time = time.time()
             per_iter_ptime = iter_end_time - iter_start_time
